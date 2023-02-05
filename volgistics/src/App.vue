67d640f6cbd9bb0352ca70f0355aa52ba0c5337c
stegmannoah@gmail.com <stegmannoah@gmail.com>
2023-02-04 18:53:25 -0800

    Logistic Calendar App start

---
+++ volgistics/src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { useCalendarStore } from '../src/stores/calendar'
+
+const storeCalendar = useCalendarStore();
+
+
+</script>
+
+<template>
+<div class="container">
+  <div class="wrapper">
+    <div class="inputs">
+      <input type="number" v-model="storeCalendar.year"/>
+      <select id="month" v-model="storeCalendar.month">
+        <option value="1">January</option>
+        <option value="2" selected>February</option>
+        <option value="3">March</option>
+        <option value="4">April</option>
+        <option value="5">May</option>
+        <option value="6">June</option>
+        <option value="7">July</option>
+        <option value="8">August</option>
+        <option value="9">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+      </select>
+<!--       <button @click="storeCalendar.getDays">Submit</button> -->
+    </div>
+    <div class="month">
+      <div class="day" v-for="n in parseInt(storeCalendar.totalDays)" :key="n">
+        <div>{{ n }}</div>
+        <div>{{ storeCalendar.getDay(n) }}</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+
+.inputs{
+  margin: 15px;
+}
+
+.wrapper{
+  margin-top: 5vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 95vw;
+  min-height: 90vh;
+  background-color: rgb(201, 245, 201);
+  border-radius: 25px;
+}
+  .month{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  .day{
+    margin: 15px;
+    background-color: rgb(185, 185, 247);
+    height: 150px;
+    width: 150px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  .container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ volgistics/src/stores/calendar.js
@@ -0,0 +1,40 @@
+import { defineStore } from "pinia"
+
+const d = new Date();
+
+export const useCalendarStore = defineStore({
+    id: "calendar",
+    state: () => ({
+        days: 0,
+        year: 2023,
+        month: d.getMonth() + 1
+    }),
+    actions: {
+        getDay(d) {
+            const dow = new Date(parseInt(this.year), parseInt(this.month), parseInt(d)).getDay();
+            switch (dow) {
+                case 0:
+                    return "Sunday";
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+
+            }
+        }
+
+    },
+    getters: {
+        totalDays(state) {
+            return new Date(parseInt(state.year), parseInt(state.month), 0).getDate();
+        }
+    }
+})
